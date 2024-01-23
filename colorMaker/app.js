51304ee01c644f53be9a96c5d2f2b6f13e4909f5
ashikofficial455 <ashikofficial455@gmail.com>
2024-01-23 18:16:25 +0000

    try

--- colorMaker/app.js
+++ colorMaker/app.js
@@ -3,13 +3,27 @@ const colors = ["Green", "Red", "Blue", "Yellow", "Purple"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function() {
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+    const selectedColor = colors[randomNumber];
+
+    document.body.style.backgroundColor = selectedColor;
+    color.textContent = selectedColor;
+
+    // Call the function to copy the text content to the clipboard
+    copyTo(selectedColor);
 });
 
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
+}
+
+function copyTo(text) {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    console.log("done");
 }
